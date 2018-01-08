0583e74b6a0ab3e152efea966c4c8b3159248f4a
Ryan C Meinzer <xnuiem@users.noreply.github.com>
2018-01-08 14:45:51 -0600

    Update config.py
    RJ-10
    Removed support for the old greenhopper API

--- example/config.py
+++ example/config.py
@@ -7,11 +7,9 @@ cache_expire = 3600  # Number of seconds until expire
 jira_options = {'async': False,
                 'rest_api_version': '2',
                 'verify': False,
-                'agile_rest_path': 'greenhopper',
                 'server': 'https://jira.atlassian.net/',
                 'rest_path': 'api',
                 'check_update': False,
-                'agile_rest_api_version': '1.0',
                 'client_cert': None,
                 'context_path': '/',
                 'headers': {
