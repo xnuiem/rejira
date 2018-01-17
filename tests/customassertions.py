b3ad015910e9afbd1b50084770fab00af9790fb4
Xnuiem <ryan.meinzer@xmtek.net>
2018-01-17 14:26:59 -0600

    RJ-23 added first unit test and started CircleCI integration

---
+++ tests/customassertions.py
@@ -0,0 +1,1 @@
+class CustomAssertions:    def assertAttrExists(self, obj, attr):        if hasattr(obj, attr):            pass        else:            raise AssertionError("Object does not have attribute " + attr)    def assertAttrNotExists(self, obj, attr):        if hasattr(obj, attr):            raise AssertionError("Object has attribute " + attr)        pass
