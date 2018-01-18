b738fdb397f88eec0d25f855846599e519e93a59
Xnuiem <ryan.meinzer@xmtek.net>
2018-01-17 22:12:10 -0600

    RJ-28 Moved config options, most of them, to environment variables

--- example/config.py
+++ example/config.py
@@ -1,12 +1,5 @@
-cache_host = "localhost"
-cache_port = 6379
-cache_db = 0
-cache_on = True
-cache_expire = 3600  # Number of seconds until expire
-
 jira_options = {"async": False,
                 "rest_api_version": "2",
-                "server": "https://jira.atlassian.net/",
                 "rest_path": "api",
                 "client_cert": None,
                 "context_path": "/",
@@ -17,13 +10,10 @@ jira_options = {"async": False,
                 },
                 "resilient": True
                 }
-jira_user = ""
-jira_pass = ""
 jira_lib = False  # Use the JIRA Library (https://github.com/pycontribs/jira)
-
-logging_level = "DEBUG"
-logging_file = ""
 
 map_fields = False
 map_raw = False
+
+create_test_file = False
 
--- rejira/__init__.py
+++ rejira/__init__.py
@@ -1,16 +1,19 @@
-from rejira.lib.cache import Cache
 import logging
+import os
 
+from rejira.lib.cache import Cache
 
 class ReJIRA:
 
     def __init__(self, config, field_map):
         self.config = config
-        logging.basicConfig(level=config.logging_level)
+        self.setup_config()
+
+        logging.basicConfig(level=self.config.logging_level)
         logger = logging.getLogger(__name__)
 
         if config.logging_file:
-            handler = logging.FileHandler(config.logging_file)
+            handler = logging.FileHandler(self.config.logging_file)
             handler.setLevel(logging.INFO)
             formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', "%Y-%m-%d %H:%M:%S")
             handler.setFormatter(formatter)
@@ -20,14 +23,27 @@ class ReJIRA:
         self.logger.info('Starting')
         self.logger.debug('===========================')
         self.logger.debug('Configuration')
-        self.logger.debug('LOGGING level:%s, file:%s', config.logging_level, config.logging_file)
-        self.logger.debug('CACHE host:%s, port:%s, db:%s, expire:%s, toggle:%s', config.cache_host, config.cache_port,
-                          config.cache_db, config.cache_expire, config.cache_on)
+        self.logger.debug('LOGGING level:%s, file:%s', self.config.logging_level, self.config.logging_file)
+        self.logger.debug('CACHE host:%s, port:%s, db:%s, expire:%s, toggle:%s', self.config.cache_host, self.config.cache_port,
+                          self.config.cache_db, self.config.cache_expire, self.config.cache_on)
 
-        self.logger.debug('MAPPING fields:%s, raw:%s', config.map_fields, config.map_raw)
-        self.logger.debug('JIRA lib:%s, options:%s', config.jira_lib, config.jira_options)
+        self.logger.debug('MAPPING fields:%s, raw:%s', self.config.map_fields, self.config.map_raw)
+        self.logger.debug('JIRA lib:%s, options:%s', self.config.jira_lib, self.config.jira_options)
         self.logger.debug('===========================')
-        self.cache = Cache(config, self.logger, field_map)
+        self.cache = Cache(self.config, self.logger, field_map)
+
+    def setup_config(self):
+        self.config.cache_host = os.getenv('REJIRA_CACHE_HOST', 'localhost')
+        self.config.cache_port = os.getenv('REJIRA_CACHE_PORT', 6379)
+        self.config.cache_db = os.getenv('REJIRA_CACHE_DB', 0)
+        self.config.cache_on = os.getenv('REJIRA_CACHE_ON', True)
+        self.config.cache_expire = os.getenv('REJIRA_CACHE_EXPIRE', 3600)
+        self.config.jira_user = os.getenv('REJIRA_JIRA_USER', '')
+        self.config.jira_pass = os.getenv('REJIRA_JIRA_PASS', '')
+        self.config.jira_options["server"] = os.getenv('REJIRA_JIRA_SERVER', '')
+        self.config.logging_level = os.getenv('REJIRA_LOGGING_LEVEL', 'DEBUG')
+        self.config.logging_file = os.getenv('REJIRA_LOGGING_FILE', '')
+
 
     def get(self, key):
         """Fetches a single issue, returning it as a dict.
