4d9fd947fff3840e93986a1402d49e7bbfd4c44e
Xnuiem <ryan.meinzer@xmtek.net>
2018-01-18 00:28:37 -0600

    RJ-29 adding more unit tests

--- example/main.py
+++ example/main.py
@@ -1,6 +1,14 @@
 import config, fields
-from rejira import ReJIRA
+import os, inspect, sys
 from pprint import pprint
+
+cmd_folder = os.path.abspath(os.path.join(os.path.split(inspect.getfile(
+    inspect.currentframe()))[0], ".."))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+from rejira import ReJIRA
+
 
 rj = ReJIRA(config, fields.field_map)
 
