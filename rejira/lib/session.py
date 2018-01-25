e6dd3f5a0776db229e97e345d9e79a7f8d23cb5d
Ryan C Meinzer <xnuiem@users.noreply.github.com>
2018-01-25 10:50:22 -0600

    Update session.py
    adding support for proxies

--- rejira/lib/session.py
+++ rejira/lib/session.py
@@ -8,3 +8,5 @@ class Session:
         self.s = requests.Session()
         self.s.auth = (config.jira_user, config.jira_pass)
         self.s.headers.update(config.jira_options["headers"])
+        if len(config.proxies) != 0:
+            self.s.proxies.update(config.proxies)
