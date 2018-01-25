fef0297b93aa0877637f4a87857d36c5c22b89ed
Ryan C Meinzer <xnuiem@users.noreply.github.com>
2018-01-25 10:52:21 -0600

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -65,6 +65,14 @@ class ReJIRAUnitTest(unittest.TestCase, CustomAssertions):
         config.jira_options["server"] = os.getenv('REJIRA_JIRA_SERVER', '')
         config.logging_level = os.getenv('REJIRA_LOGGING_LEVEL', 'ERROR')
         config.logging_file = os.getenv('REJIRA_LOGGING_FILE', '')
+        http_proxy = os.getenv('REJIRA_HTTP_PROXY', None)
+        https_proxy = os.getenv('REJIRA_HTTPS_PROXY', None)
+
+        if http_proxy is not None:
+            config.proxies.append(http_proxy)
+
+        if https_proxy is not None:
+            config.proxies.append(https_proxy)
 
         return config
 
