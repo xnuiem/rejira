b182a65d2fe3c90de598860c441f9f6acf3a36a6
Xnuiem <ryan.meinzer@xmtek.net>
2018-02-03 22:25:20 -0600

    adding workflows

--- setup.sh
+++ setup.sh
@@ -2,3 +2,10 @@
 
 sed -i -e 's/<USERNAME>/'"$PYPI_USERNAME"'/g' .pypirc
 sed -i -e 's/<PASSWORD>/'"$PYPI_PASSWORD"'/g' .pypirc
+
+cp .pypirc ~
+python setup.py sdist
+python setup.py bdist_wheel
+twine upload dist/*
+rm -rf ~/.pypirc
+rm -rf .pypirc
